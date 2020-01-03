144599b6c3086d8e4075561b5e7ec2b2158d564c
timbmedcalf <timmedcalf@pop-os.localdomain>
2020-01-03 19:57:07 +0000

    Styled header

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h2 className='logo'>AlgoPicker</h2>
+      <button className='hamburger'>
+        <span></span>
+      </button>
+    </div>
+  );
+}
+
+export default Header;
