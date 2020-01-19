2da0b73b7de0490309cb9b4f6000d67b48289b4a
Tim <timbmedcalf@gmail.com>
2020-01-19 18:58:18 +0000

    Decision tree now uses pointers from the word to go to the next node

--- src/pages/TheDecisionTree.js
+++ src/pages/TheDecisionTree.js
@@ -1,54 +1,73 @@
-import React, { useState } from 'react';
-import Typest from '../components/Typer/Typest';
-import UserDecision from '../components/Typer/UserDecision';
+import React, { useState } from "react";
+import Typest from "../components/Typer/Typest";
+import UserDecision from "../components/Typer/UserDecision";
 
 function TheDecisionTree() {
-  const [decisionText, setDecisionText] = useState('');
+  const [decisionText, setDecisionText] = useState("");
   const [decisionIndex, setDecisionIndex] = useState(0);
   const [usersDecisions, setUserDecisions] = useState([]);
+  const [complete, setComplete] = useState(false);
 
   //Gets the button value of the users decision
   const handleDecision = e => {
-    const decision = e.target.innerHTML.trim()
+    const decision = e.target.innerHTML.trim();
     setDecisionText(decision);
     setUserDecisions([...usersDecisions, decision]);
+
     //Increments the level of the decision tree the user is on
-    if (decisionIndex < userTree.length - 1) {
-      setDecisionIndex(decisionIndex + 1);
+    if (userTree[decisionIndex].decisions.pointers) {
+      let nextDecision = findNextDecision(decision);
+      if (nextDecision !== null) {
+        setDecisionIndex(nextDecision);
+      } else {
+        setDecisionIndex(decisionIndex + 1);
+      }
+    } else {
+      setComplete(true);
     }
+  };
+
+  const findNextDecision = decision => {
+    //Get the index of the word then use that index to find where the next node is with the pointers
+    let nextDecision = userTree[decisionIndex].decisions.words.indexOf(
+      decision
+    );
+    if (nextDecision === -1) return null;
+    return userTree[decisionIndex].decisions.pointers[nextDecision];
   };
 
   const userTree = [
     {
       sentences: [
-        'Hello and welcome to my decision tree!',
-        'Are you looking to store, search or sort data?'
+        "Hello and welcome to my decision tree!",
+        "Are you looking to store, search or sort data?"
       ],
       decisions: {
         sentence: 1,
-        words: ['store', 'search', 'sort']
+        words: ["store", "search", "sort"],
+        pointers: [1, 2, 1]
       }
     },
     {
-      sentences: ['This is test number 3!'],
+      sentences: ["This is test number 3!"],
       decisions: {
         sentence: 0,
-        words: ['test', 'this']
+        words: ["test", "this"]
       }
     },
     {
-      sentences: ['This is test number 4'],
+      sentences: ["This is test number 4"],
       decisions: {
         sentence: 0,
-        words: ['4']
+        words: ["4"]
       }
-    },
+    }
   ];
 
   return (
-    <div className='the-decision-tree'>
-      <section className='typewritter'>
-        <h2 className='typewriter-text'>
+    <div className="the-decision-tree">
+      <section className="typewritter">
+        <h2 className="typewriter-text">
           <Typest
             key={`typer: ${decisionIndex}`}
             sentences={userTree[decisionIndex].sentences}
