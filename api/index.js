c008e093f2bbcf5d05935abf7526e20df13c4535
timbmedcalf <timbmedcalf@gmail.com>
2020-03-05 08:03:28 +0000

    Error handled db connection

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.DB_URL, {
 
 const db = mongoose.connection
 
+db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
